5817b7d19294a8ddc8302fa63de510b3b78b603d
Ademilton Marcelo Nunes <52976638+ademiltonnunes@users.noreply.github.com>
2024-03-03 00:42:21 -0800

    wk8

--- Lectures/Week8-GetPost/index.js
+++ Lectures/Week8-GetPost/index.js
@@ -1,37 +1,23 @@
-// Import Express module
 const express = require('express');
 const app = express();
-
-// Importing for https protocol
 const https = require('https');
-
-// Importing for file system to read any file in the repository
-// We use it to read the key stored in the repository
 const fs = require('fs');
 const { start } = require('repl');
-
-//Reading the key and certificate
-const key = fs.readFileSync('./SSL/server.key');
-const cert = fs.readFileSync('./SSL/server.crt');
-
-// Another way to store the key and certificate
 const httpsOptions = {
     key: fs.readFileSync('./SSL/server.key'),
     cert: fs.readFileSync('./SSL/server.crt')
 };
-
-//Parse the body in Json
-app.use(express.json());
+//Routes
+const startup = require('./routes/startup');
+const classSchedule = require('./routes/classSchedule');
+const location = require('./routes/location');
 
 //Creating server
 const server = https.createServer(httpsOptions,app);
-
-//Adding the routes
-const startup = require('./routes/startup');
-
-//We also can include more components to the routes
+app.use(express.json());
 app.use('/', startup);
-app.use('/test', startup); //different route name to the same component
+app.use('/classschedule', classSchedule);
+app.use('/location', location);
 
 server.listen(8080, () => {
     console.log('Server running at https://localhost:8080/');
--- Lectures/Week8-GetPost/routes/classSchedule.js
+++ Lectures/Week8-GetPost/routes/classSchedule.js
@@ -1,7 +1,89 @@
 const express = require('express');
+
+// Importing data
+const data = require('../db/class-schedule.json');
 const classSchedule = express.Router();
 
-// our endpoints 
+const fs = require('fs');
+
+
+// 1. Get all courses
+classSchedule.get('/courses', (req, res) => {
+    res.json(data);
+});
+
+// 2. Get with parameters to get a specific course
+classSchedule.get('/courses/:course', (req, res) => {
+    // reading the paramenter passed
+    const course = req.params.course;
+
+    // finding the course
+    const foundCourse = data.find(c=> c["Course"] === course);
+
+    if (foundCourse) {
+        res.json(foundCourse);
+    } else {
+        res.status(404).send('Course not found');
+    }
+});
+
+// 3. GET with query parameters online courses
+classSchedule.get('/online', (req, res) => {
+
+    // filtering the data
+    const filteredCourse = data.filter(c=> c.Classroom.includes("Online"));
+
+    if (filteredCourse) {
+        res.json(filteredCourse);
+    } else {
+        res.status(404).send('Course not found');
+    }
+});
+
+// 4. POST where the classroom is assigment
+classSchedule.post('/classroom', (req, res) => {
+    const {course} = req.body;
+
+    if (!course) {
+        return res.status(400).json({message: 'Course is required'});
+    }
+
+    const found = data.find(c => c.Course === course);
+
+    if (found) {
+        res.json({classroom: found?.Classroom});
+    } else {
+        res.status(404).send('Classroom not found');
+    }
+});
+
+// 5. PUT to update the classroom
+classSchedule.put('/update/:course', (req, res) => { 
+    const course = req.params.course;
+    const {newInstructor, token} = req.body;
+
+    // token value = admin
+    if (token !== "admin") {
+        return res.status(401).json({message: 'Unauthorized'});
+    }
+
+    const indexList = data.findIndex(c => c.Course === course);
+
+    console.log(indexList);
+
+    data[indexList].Instructor = newInstructor;
+    const filePath = path.join(__dirname, '../db/updated-class-schedule.json','class-schedule');
+    fs.writeFileSync(filePath, JSON.stringify);
+
+    res.json({message: 'Instructor updated'});
+
+});	
+
+
+
+// 6. DELETE to remove a class
+
+
 
 
 module.exports = classSchedule;
