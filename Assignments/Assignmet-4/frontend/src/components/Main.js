41815ecd307701b513f7af81cbb0232eb8ab9915
Ademilton Marcelo Nunes <52976638+ademiltonnunes@users.noreply.github.com>
2024-03-25 14:25:04 -0700

    updates

--- Assignments/Assignmet-4/frontend/src/components/Main.js
+++ Assignments/Assignmet-4/frontend/src/components/Main.js
@@ -1,46 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import StoreList from './StoreList';
-import StoreFilter from './StoreFilter';
-
 
 const Main = () => {
-
   const [stores, setStores] = useState([]);
-  const [filteredStores, setFilteredStores] = useState([]);
+  const [zipcode, setZipcode] = useState('');
 
   useEffect(() => {
     fetchAllStores();
   }, []);
 
   const fetchAllStores = async () => {
-
-    try{      
-      const response = await axios.get('https://localhost:8080/store');
+    try {
+      const response = await axios.get('https://localhost:8080/store', { rejectUnauthorized: false });
       setStores(response?.data.ecomStores);
-      setFilteredStores(response?.data.ecomStores);
-    
-    }catch(error){
+    } catch (error) {
       console.error('Error fetching all stores:', error);
     }
- 
   }
-  const filterStoresByZipcode = (zipcode) => {
-    axios.post('https://localhost:8080/store/zipcode', { zipCode: zipcode })
-      .then(response => {
-        setFilteredStores(response.data.ecomStores);
-      })
-      .catch(error => {
-        console.error('Error filtering stores by ZIP code:', error);
-      });
-  };
+
+  const filterStoresByZipcode = async () => {
+    try {
+
+      if (!zipcode) {
+        fetchAllStores();
+        return;
+      }
+      
+      const response = await axios.post('https://localhost:8080/store/zipcode', { zipCode: zipcode });
+      console.log('response:', response);
+      setStores(response?.data);
+    } catch (error) {
+      console.error('Error filtering stores by ZIP code:', error);
+    }
+  }
+  
 
   return (
     <div className="app">
       <h1>Safeway - Grocery Stores</h1>
-      <StoreFilter onFilter={filterStoresByZipcode} />
-      <StoreList stores={filteredStores} />
+      <div className="store-filter">
+        <h2>Filter by ZIP Code</h2>
+        <input
+          type="text"
+          placeholder="Enter ZIP Code"
+          value={zipcode}
+          onChange={(e) => setZipcode(e.target.value)}
+        />
+        <button onClick={filterStoresByZipcode}>Filter</button>
+      </div>
+      <StoreList stores={stores} />
     </div>
   );
 };
+
 export default Main;
